b421e426b6a6a0602306508cbe885e838a35d773
marcos5163 <marcoschr56@gmail.com>
2023-12-02 13:43:27 +0530

    swagger

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 from app.views import GameViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Game CRUD API",
+      default_version='v1',
+      description="API for managing game records, allowing CRUD operations to create, retrieve, update and delete game information.",
+      contact=openapi.Contact(email="marcoschr56@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
 
  path('game/', GameViewSet.as_view({"post":"create", "get": "list"}), name="game_api_view"),
  path('<uuid:game_uuid>/game/', GameViewSet.as_view({"get":"get", "patch": "update", "delete":"delete"}), name="game_api_view"),
   
+]
+urlpatterns += [
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- svg_assignment/settings.py
+++ svg_assignment/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = 'svg_assignment.urls'
