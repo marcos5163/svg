0f76d12c189e55a655f033266d7af250c2fcff6c
marcos5163 <marcoschr56@gmail.com>
2023-12-02 14:05:11 +0530

    fix

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Game CRUD API",
       default_version='v1',
-      description="API for managing game records, allowing CRUD operations to create, retrieve, update and delete game information.",
+      description="Swagger for managing game records",
       contact=openapi.Contact(email="marcoschr56@gmail.com"),
    ),
    public=True,
@@ -25,3 +25,4 @@ urlpatterns += [
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
